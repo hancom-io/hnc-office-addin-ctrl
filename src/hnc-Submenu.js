745e08aca15b87db6d4fdf35bdb0d2cd474bec24
정원석 <wsjung@hancom.com>
2020-11-14 20:56:28 +0900

    submenuButton, submenu에 submenu 키보드 이벤트로 show/hide 구현

--- src/hnc-Submenu.js
+++ src/hnc-Submenu.js
@@ -1,22 +1,119 @@
 // Copyright (c) Hancom. All rights reserved. 
 // Licensed under the MIT License. See License.txt in the project root for license information. 
 
-
-// expander 추가
+// 키보드 이벤트 및 expander 추가
 function hncInitSubmenu() {
-    hncAddSubmenuButtonExpander();
+    hncInitSubmenuButton();
     hncAddSubmenuItemExpander();
 }
 
-// submenu-button들의 아래쪽 Expander 추가
-function hncAddSubmenuButtonExpander() {
+// 키보드 이벤트와 submenu-button들의 아래쪽 Expander 추가
+function hncInitSubmenuButton() {
     let buttons = document.querySelectorAll('.hnc-submenu-button');
 
     // 하위 depth 부터 넣어야 모두 보임. 0부터 삽입하면 하위 depth 의 expander가 안보임
     for (let i = buttons.length; 0 < i ; i--) {
         buttons[i - 1].innerHTML += '<div class="hnc-expander-down"><svg width="7" height="4"><g><path class="hnc-expander-path" d="M0,0 L7,0 L3.5,4z"/></g></svg></div>';
     }
+
+    hncInitSubmenuButtonKeyboardEvent(buttons);
 }
+// submenuButton 에서의 키보드 이벤트 
+function hncInitSubmenuButtonKeyboardEvent(buttons) {
+    if (buttons == null) {
+        return;
+    }
+    Array.prototype.forEach.call(buttons, function(button, i)  {
+
+        // 각 submenu 이벤트 초기화
+        let submenu = button.querySelector('.hnc-submenu');
+        hncInitSubmenuKeyboardEvent(submenu);
+
+        // button Keydown
+        button.addEventListener('keydown', function(e) {
+            switch (e.key) {
+                case 'ArrowDown': // 메뉴를 연다.
+                {
+                    hncShowSubmenu(submenu);
+                    e.preventDefault(); // 이벤트 전파를 중단하여 스크롤이 되지 않게 함
+                    break;
+                }
+            }
+        });
+    });
+}
+
+// submenu에서의 키보드 이벤트
+function hncInitSubmenuKeyboardEvent(submenu) {
+    if (submenu == undefined) {
+        return;
+    }
+
+    let menuItemCount = hncGetMenuItemCount(submenu);
+
+    // submenu Keydown
+    submenu.addEventListener('keydown', function(e) {
+        switch (e.key) { 
+            case 'ArrowUp': // 메뉴를 닫는다.
+            case 'Escape': // 메뉴를 닫는다.
+            { 
+                if (hncIsVisibleSubmenu(submenu) == true) { // 열려진 상태에서 1회만 처리하고 이벤트 중단. 따라서 계속 up 키를 누르면 스크롤 될 것임
+                    hncHideSubmenu(submenu);
+                    e.preventDefault(); // 이벤트 전파를 중단
+                }
+                break;
+            }
+            case 'Tab': // shift+tab도 동일
+            { 
+                if (hncIsVisibleSubmenu(submenu) == true) { // e.preventDefault(); 를 호출하지 않고 이벤트 전파하여 tab이동시킴
+                    hncHideSubmenu(submenu);
+                }
+                break;
+            }         
+        }  
+    });
+
+    // // 메뉴 하위의 li에 이벤트 핸들러 장착 
+    // let lis = submenu.querySelectorAll('li');
+    // Array.prototype.forEach.call(lis, function(li, i) {
+    //     if (hncIsSubmenuItem(li) == true) { // submenuItem이면 하위 submenu에도 이벤트 등록
+
+
+    //     }
+    //     // 일반 menuItem
+    //     else {
+    //         li.addEventListener('keydown', function(e) {
+    //             // 현재 hover 표시된 위치를 구함
+    //             // 전체 갯수를 구함
+    //             // 내부에서 rotate
+                
+    //             switch (e.key) {
+
+    //             }
+    //             alert("test");
+    //         });
+
+    //     }
+    // });
+}
+
+function hncGetMenuItemCount(submenu) {
+    if (submenu == null) {
+        return 0;
+    }
+    return submenu.childElementCount;
+}
+
+function hncIsSubmenuItem(li) {
+    if (li == undefined) {
+        return false;
+    }   
+    if (li.className.includes('hnc-submenu-item') == true) {
+        return true;
+    }
+    return false;
+}
+
 // submenu-item들의 오른쪽 Expander 추가. absolute로 배치됨
 function hncAddSubmenuItemExpander() {
     let items = document.querySelectorAll('.hnc-submenu-item');
@@ -34,22 +131,57 @@ function onClick_HncSubmenuButton(e) {
     if (submenu == undefined) {
         return;
     }
-    let opacity = submenu.style.opacity;
-    if (opacity == 1) {
-        submenu.style.opacity = 0;
-        submenu.style["pointer-events"] = 'none';
-        // submenu.stype.pointer-events = none;
-    }
-    else {
-        hncCloseAllSubmenu(); // 모든 서브메뉴를 닫고
-
-        submenu.style.opacity = 1; // 내것만 연다.
-        submenu.style["pointer-events"] = 'auto';
-    }
+    hncToggleSubmenu(submenu);
 
     // 이벤트 전파를 중단하여, $("body, html").click 이벤트에서 닫히지 않게 한다.
     e.stopPropagation();
 }
+// submenu를 토글한다.
+function hncToggleSubmenu(submenu) {
+    if (submenu == undefined) {
+        return;
+    }
+    
+    if (hncIsVisibleSubmenu(submenu) == true) { // 화면에서 보인다면 감춤 */
+        hncHideSubmenu(submenu);
+    }
+    else {
+        hncShowSubmenu(submenu);
+    }  
+}
+function hncIsVisibleSubmenu(submenu) {
+    if (submenu == undefined) {
+        return false;
+    }
+    let opacity = submenu.style.opacity;
+    if (opacity == 1) {
+        return true;
+    }
+    return false;
+}
+// show시 키보드 포커스 획득
+function hncShowSubmenu(submenu) {
+    if (submenu == undefined) {
+        return;
+    }
+    hncCloseAllSubmenu(); // 모든 서브메뉴를 닫고
+
+    submenu.style.opacity = 1; // 내것만 연다.
+    submenu.style["pointer-events"] = 'auto'; 
+    submenu.tabIndex = 0; // 키보드 이벤트를 받을 수 있게 함
+    submenu.focus(); 
+}
+// hide시 키보드 포커스 반환
+function hncHideSubmenu(submenu) {
+    if (submenu == undefined) {
+        return;
+    }
+    submenu.style.opacity = 0;
+    submenu.style["pointer-events"] = 'none';   
+    submenu.tabIndex = -1; // 키보드 이벤트를 받을 수 없게 함
+    submenu.parentElement.focus();
+}
+
 // 하위 메뉴 클릭시 이벤트 전파 중단하여 닫히지 않게 함
 function onClick_HncSubmenuItem(e) {
     e.stopPropagation();
@@ -73,6 +205,14 @@ function onHover_HncSubmenuItem(e) {
 
     submenu.style.left = String(parentRight) + 'px' ;
     submenu.style.top = String(parentTop) + 'px';
+
+    // hover시 보이는 것은 css에서 해도 되나 focus 처리를 위해 javascript 사용
+    //!! hncShowSubmenu(submenu); 자신의 parent라면 닫으면 안된다.
+    submenu.style.opacity = 1; // 내것만 연다.
+    submenu.style["pointer-events"] = 'auto'; 
+    submenu.tabIndex = 0; // 키보드 이벤트를 받을 수 있게 함
+    submenu.focus(); 
+
 }
 // body, html 클릭시 모든 submenu 닫음
 function onClick_HncDocument(object, e) {
@@ -82,7 +222,11 @@ function hncCloseAllSubmenu() {
     let others = document.querySelectorAll('.hnc-submenu');
 
     for (let other of others) {
-        other.style.opacity = 0;
-        other.style["pointer-events"] = 'none';
+        if (hncIsVisibleSubmenu(other) == true) {
+            other.style.opacity = 0;
+            other.style["pointer-events"] = 'none';
+            other.tabIndex = -1; // 키보드 이벤트를 받을 수 없게 함
+            other.parentElement.focus();
+        }
     }
 }
